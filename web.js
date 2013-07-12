5efbb5c615fea38c1c8bd28a2bcd54f89e819dc2
anandraveendran <saytoanand@gmail.com>
2013-07-12 03:01:40 +0000

    hi5

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var buffer = new Buffer(20);
-fs.readFileSync('/index.html', function (err, data) {
+fs.readFileSync('index.html', function (err, data) {
   if (err) throw err;
   console.log(data);
 response.send(buffer.toString('utf8', 0, buffer.length));
