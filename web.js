5e00878eedab934a5af768e69aef2f2d21f180d7
anandraveendran <saytoanand@gmail.com>
2013-07-12 03:30:32 +0000

    hi

--- web.js
+++ web.js
@@ -6,12 +6,8 @@ app.get('/', function(request, response) {
 var buffer = new Buffer(20);
 fs.readFileSync('index.html', 'utf8',function (err, data) {
   if (err) throw err;
-  console.log(data);
+  console.log(data);});
 response.send(buffer.toString('utf8'));
-});
-
-});
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
