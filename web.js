1c5aa7f6661789e02dc775d579731f43f8660855
anandraveendran <saytoanand@gmail.com>
2013-07-12 03:33:47 +0000

    hi

--- web.js
+++ web.js
@@ -4,9 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var buffer = new Buffer(20);
-fs.readFileSync('index.html', 'utf8',function (err, data) {
-  if (err) throw err;
-  console.log(data);});
+buffer = fs.readFileSync('index.html', 'utf8');
 response.send(buffer.toString('utf8'));
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
