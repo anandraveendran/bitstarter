d8e6cd5311ef1b7aaaabac3effad1331bff76084
anandraveendran <saytoanand@gmail.com>
2013-07-12 02:56:06 +0000

    hi3

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var buffer = new Buffer(20);
+fs.readFileSync('/index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+response.send(buffer.toString('utf8', 0, buffer.length));
+});
+
 });
 
 var port = process.env.PORT || 5000;
